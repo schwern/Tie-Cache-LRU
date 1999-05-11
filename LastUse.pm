063343eddefb0a592a62dfbf4cfa5435b5f6423a
schwern <schwern@8151f2b9-fde8-0310-94fd-f048d12aab9e>
1999-05-11 21:11:30 +0000

    Initial revision
    git-svn-id: http://svn.schwern.org/repos/CPAN/Tie-Cache-LRU/trunk@2993 8151f2b9-fde8-0310-94fd-f048d12aab9e

---
+++ LastUse.pm
@@ -0,0 +1,263 @@
+package Tie::Cache::InMemory::LastUse;
+
+require 5.00502;
+
+use Carp::Assert;
+#use base qw(Tie::Cache::LastUse Tie::Cache::InMemory);
+use vars qw($VERSION);
+BEGIN { 
+    $VERSION = 0.01 
+}
+
+use constant SUCCESS => 1;
+use constant FAILURE => 0;
+
+# Node members.
+use enum qw(KEY VALUE PREV NEXT);
+
+=head1 NAME
+
+Tie::Cache::InMemory::LastUse - An in memory version of Tie::Cache::LastUse
+
+
+=head1 SYNOPSIS
+
+	tie %cache, 'Tie::Cache::InMemory::LastUse', 500;
+	tie %cache, 'Tie::Cache::InMemory::LastUse', '400k'; #UNIMPLEMENTED
+
+# Otherwise exactly like Tie::Cache.
+
+
+=head1 DESCRIPTION
+
+This is an implementation of a last-use cache keeping the cache in RAM
+rather than on disk.  See Tie::Cache::LastUse for details.
+
+NOTE:  When the cache size is limited by the size of the data, this module
+does uses the size of the data entered into the cache.  The cache data
+structure may (will) be larger than the cache size.  Such is Perl.
+
+=cut
+
+sub TIEHASH {
+	my($class, $max_size) = @_;
+	my $self = {};
+	
+	bless $self, $class;
+	
+	$self->_init;
+	$self->{max_size} = $max_size;
+
+	return $self;
+}
+
+
+sub FETCH {
+	my($self, $key) = @_;
+	
+	return unless $self->EXISTS($key);
+	
+	my $node = $self->{index}{$key};
+	$self->_promote($node);
+	return $node->[VALUE];
+}
+
+
+sub STORE {
+	my($self, $key, $value) = @_;
+
+	if( $self->EXISTS($key) ) {
+		my $node = $self->{index}{$key};
+		$node->[VALUE] = $value;
+		$self->_promote($node);
+	}
+	else {
+		my $node = [];
+		@{$node}[KEY, VALUE] = ($key, $value);
+		
+		# Make ourselves the freshest.
+		if(defined $self->{freshest} ) {
+			$self->{freshest}->[NEXT] = $node;
+			$node->[PREV] = $self->{freshest};
+		}
+		else {
+			assert($self->{size} == 0);
+		}
+		$self->{freshest} = $node;
+		
+		# If we're the first node, we are stinky, too.
+		unless( defined $self->{stinkiest} ) {
+			assert($self->{size} == 0);
+			$self->{stinkiest} = $node;
+		}
+		$self->{size}++;
+		$self->{index}{$key} = $node;
+		$self->_cull;
+	}
+	return SUCCESS;
+}
+
+
+sub EXISTS {
+	my($self, $key) = @_;
+	
+	return exists $self->{index}{$key};
+}
+
+
+sub CLEAR {
+	my($self) = @_;
+	$self->_init;
+}
+
+
+sub DELETE {
+	my($self, $key) = @_;
+	
+	return unless $self->EXISTS($key);
+	
+	my $node = $self->{index}{$key};
+	$self->{freshest}  = $node->[PREV] if $self->{freshest}  == $node;
+	$self->{stinkiest} = $node->[NEXT] if $self->{stinkiest} == $node;
+	$self->_yank($node);
+	delete $self->{index}{$key};
+	
+	$self->{size}--;
+	
+	return SUCCESS;
+}
+
+
+# keys() should return most to least recent.
+sub FIRSTKEY {
+	my($self) = shift;
+	my $first_node = $self->{freshest};
+	assert($self->{size} == 0 xor defined $first_node);
+	return $first_node->[KEY];
+}
+
+sub NEXTKEY  {
+	my($self, $last_key) = @_;
+	my $last_node = $self->{index}{$last_key};
+	assert(defined $last_node);
+	return defined $last_node ? $last_node->[PREV][KEY]
+							  : undef;
+}
+
+# The chain must be broken.
+sub DESTROY  {
+	my($self) = shift;
+	
+	for(my $node = $self->{freshest}; defined $node; ) {
+		my $prev_node = $node->[PREV];
+		$node->[PREV] = undef;
+		$node->[NEXT] = undef;
+		$node = $prev_node;
+	}
+	
+	$self->_init;
+	
+	return SUCCESS;
+}
+
+sub _init {
+	my($self) = shift;
+	
+	$self->{cache} = undef;
+	$self->{freshest} = undef;
+	$self->{stinkiest}   = undef;
+	$self->{index} = {};
+	$self->{size} = 0;
+	
+	return SUCCESS;
+}
+
+
+sub _yank {
+	my($self, $node) = @_;
+	
+	my $prev_node = $node->[PREV];
+	my $next_node = $node->[NEXT];
+	$prev_node->[NEXT] = $next_node if defined $prev_node;
+	$next_node->[PREV] = $prev_node if defined $next_node;
+
+	$node->[NEXT] = undef;
+	$node->[PREV] = undef;
+
+	return SUCCESS;
+}
+
+
+sub _promote {
+	my($self, $node) = @_;
+	
+	# _promote can take a node or a key.  Get the node from the key.
+	$node = $self->{index}{$node} unless ref $node;
+	return unless defined $node;
+	
+	# Don't bother if there's only one node, or if this node is
+	# already the freshest.
+	return if $self->{size} == 1 or $self->{freshest} == $node;
+	
+	# Pull the $node out of its position.
+	$self->_yank($node);
+	
+	# Place the $node at the head.
+	my $old_head  = $self->{freshest};
+	$old_head->[NEXT]  = $node;
+	$node->[PREV]      = $old_head;
+	$node->[NEXT]      = undef;
+
+	$self->{freshest} = $node;
+	
+	# On the off chance that we just promoted the stinkiest node, make sure
+	# the stinkiest pointer is updated.
+	if( $self->{stinkiest} == $node ) {
+		assert(not defined $node->[PREV]);
+		$self->{stinkiest} = $node->[NEXT];
+	}
+	
+	return SUCCESS;
+}
+
+
+sub _cull {
+	my($self) = @_;
+	
+	# Could do this in one step, but it makes sizing the cache by
+	# memory use (not # of items) more difficult.
+	my $max_size = $self->{max_size};
+	for( ;$self->{size} > $max_size; $self->{size}-- ) {
+		my $rotten = $self->{stinkiest};
+		assert(!defined $rotton->[PREV]);
+		my $new_stink = $rotten->[NEXT];
+		
+		$rotten->[NEXT]    = undef;
+		$new_stink->[PREV] = undef;
+		
+		$self->{stinkiest} = $new_stink;
+	}
+	
+	return SUCCESS;
+}
+
+=pod
+
+=head1 FUTURE
+
+Should eventually allow the cache to be in shared memory.
+
+Max size by memory use unimplemented.
+
+=head1 AUTHOR
+
+Michael G Schwern <schwern@pobox.com>
+
+
+=head1 SEE ALSO
+
+Tie::Cache::LastUse, Tie::Cache::InMemory
+
+=cut
+
+1;
---
+++ test.pl
@@ -0,0 +1,90 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
+
+######################### We start with some black magic to print on failure.
+
+# Change 1..1 below to 1..last_test_to_print .
+# (It may become useful if the test is moved to ./t subdirectory.)
+use strict;
+
+my $loaded;
+my $test_num = 1;
+BEGIN { $| = 1; $^W = 1; }
+END {print "not ok $test_num\n" unless $loaded;}
+use Tie::Cache::InMemory::LastUse;
+$loaded = 1;
+print "ok $test_num\n";
+$test_num++;
+######################### End of black magic.
+
+# Insert your test code below (better if it prints "ok 13"
+# (correspondingly "not ok 13") depending on the success of chunk 13
+# of the test code):
+sub ok {
+	my($test, $name) = shift;
+	print "not " unless $test;
+	print "ok $test_num";
+	print " - $name" if defined $name;
+	print "\n";
+	$test_num++;
+}
+
+sub eqarray  {
+	my($a1, $a2) = @_;
+	return 0 unless @$a1 == @$a2;
+	my $ok = 1;
+	for (0..$#{$a1}) { 
+	    unless($a1->[$_] eq $a2->[$_]) {
+		$ok = 0;
+		last;
+	    }
+	}
+	return $ok;
+}
+
+my %cache;
+my $tied = tie %cache, 'Tie::Cache::InMemory::LastUse', 5;
+ok(defined $tied, 'tie'); # 2
+
+$cache{foo} = "bar";
+ok($cache{foo} eq 'bar', 'basic store & fetch'); # 3
+
+ok(exists $cache{foo}, 'basic exists'); # 4
+
+$cache{bar} = 'yar';
+$cache{car} = 'jar';
+# should be car, bar, foo
+my @test_order = qw(car bar foo);
+my @keys = keys %cache;
+ok(eqarray(\@test_order, \@keys), 'basic keys'); # 5
+
+
+# Try a key reordering.
+my $foo = $cache{bar};
+# should be bar, car, foo
+@test_order = qw(bar car foo);
+@keys = keys %cache;
+ok(eqarray(\@test_order, \@keys), 'basic promote'); # 6
+
+
+# Try the culling.
+$cache{har}  = 'mar';
+$cache{bing} = 'bong';
+$cache{zip}  = 'zap';
+# should be zip, bing, har, bar, car
+@test_order = qw(zip bing har bar car);
+@keys = keys %cache;
+ok(eqarray(\@test_order, \@keys), 'basic cull'); # 7
+
+
+# Try deleting from the end.
+delete $cache{car};
+ok(eqarray([qw(zip bing har bar)], [keys %cache]), 'end delete'); # 8
+
+# Try from the front.
+delete $cache{zip};
+ok(eqarray([qw(bing har bar)], [keys %cache]), 'front delete');  # 9
+
+# Try in the middle
+delete $cache{har};
+ok(eqarray([qw(bing bar)], [keys %cache]), 'middle delete'); #10
