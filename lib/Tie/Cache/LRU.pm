ca6be3d246418afe5f1c6998d087f461d66d557c
schwern <schwern@8151f2b9-fde8-0310-94fd-f048d12aab9e>
2001-07-29 09:01:23 +0000

    Version 0.20
    Gutted and using Tie::Cache::LRU::Array instead.
    
    
    git-svn-id: http://svn.schwern.org/repos/CPAN/Tie-Cache-LRU/trunk@3021 8151f2b9-fde8-0310-94fd-f048d12aab9e

--- lib/Tie/Cache/LRU.pm
+++ lib/Tie/Cache/LRU.pm
@@ -2,22 +2,14 @@ package Tie::Cache::LRU;
 
 use strict;
 
-require 5.00502;
-
-use Carp::Assert;
+use base qw(Tie::Cache::LRU::Array);
 
 use vars qw($VERSION);
-BEGIN { 
-    $VERSION = 0.06;
+BEGIN {
+    $VERSION = '0.20';
 }
 
-use constant DEFAULT_MAX_SIZE => 500;
-
-use constant SUCCESS => 1;
-use constant FAILURE => 0;
-
-# Node members.
-use enum qw(KEY VALUE PREV NEXT);
+=pod
 
 =head1 NAME
 
@@ -26,14 +18,16 @@ Tie::Cache::LRU - A Least-Recently Used cache
 
 =head1 SYNOPSIS
 
+    use Tie::Cache::LRU;
+
     tie %cache, 'Tie::Cache::LRU', 500;
     tie %cache, 'Tie::Cache::LRU', '400k'; #UNIMPLEMENTED
 
     # Use like a normal hash.
-    
+
     $cache_obj = tied %cache;
     $current_size = $cache_obj->curr_size;
-    
+
     $max_size = $cache_obj->max_size;
     $cache_obj->max_size($new_size);
 
@@ -86,7 +80,7 @@ this:
 
 This ties a cache to %cache which will hold a maximum of $cache_size
 keys.  If $cache_size is not given it uses a default value,
-Tie::Cache::LRU::DEFAULT_MAX_SIZE.
+Tie::Cache::LRU->DEFAULT_MAX_SIZE.
 
 If the size is set to 0, the cache is effectively turned off.  This is
 useful for "removing" the cache from a program without having to make
@@ -96,129 +90,6 @@ differences with and without a cache.
 All of the expected hash operations (exists, delete, slices, etc...) 
 work on the %cache.
 
-
-=cut
-
-sub TIEHASH {
-    my($class, $max_size) = @_;
-    my $self = {};
-    
-    bless $self, $class;
-    
-    $max_size = DEFAULT_MAX_SIZE unless defined $max_size;
-
-    $self->_init;
-    $self->max_size($max_size);
-
-    return $self;
-}
-
-
-sub FETCH {
-    my($self, $key) = @_;
-    
-    return unless $self->EXISTS($key);
-    
-    my $node = $self->{index}{$key};
-    $self->_promote($node);
-    return $node->[VALUE];
-}
-
-
-sub STORE {
-    my($self, $key, $value) = @_;
-
-    if( $self->EXISTS($key) ) {
-        my $node = $self->{index}{$key};
-        $node->[VALUE] = $value;
-        $self->_promote($node);
-    }
-    else {
-        my $node = [];
-        @{$node}[KEY, VALUE] = ($key, $value);
-        
-        # Make ourselves the freshest.
-        if(defined $self->{freshest} ) {
-            $self->{freshest}->[NEXT] = $node;
-            $node->[PREV] = $self->{freshest};
-        }
-        else {
-            assert($self->{size} == 0);
-        }
-        $self->{freshest} = $node;
-        
-        # If we're the first node, we are stinky, too.
-        unless( defined $self->{stinkiest} ) {
-            assert($self->{size} == 0);
-            $self->{stinkiest} = $node;
-        }
-        $self->{size}++;
-        $self->{index}{$key} = $node;
-        $self->_cull;
-    }
-    return SUCCESS;
-}
-
-
-sub EXISTS {
-    my($self, $key) = @_;
-    
-    return exists $self->{index}{$key};
-}
-
-
-sub CLEAR {
-    my($self) = @_;
-    $self->_init;
-}
-
-
-sub DELETE {
-    my($self, $key) = @_;
-    
-    return unless $self->EXISTS($key);
-    
-    my $node = $self->{index}{$key};
-    $self->{freshest}  = $node->[PREV] if $self->{freshest}  == $node;
-    $self->{stinkiest} = $node->[NEXT] if $self->{stinkiest} == $node;
-    $self->_yank($node);
-    delete $self->{index}{$key};
-    
-    $self->{size}--;
-    
-    return SUCCESS;
-}
-
-
-# keys() should return most to least recent.
-sub FIRSTKEY {
-    my($self) = shift;
-    my $first_node = $self->{freshest};
-    assert($self->{size} == 0 xor defined $first_node);
-    return $first_node->[KEY];
-}
-
-sub NEXTKEY  {
-    my($self, $last_key) = @_;
-    my $last_node = $self->{index}{$last_key};
-    assert(defined $last_node) if DEBUG;
-
-    # NEXTKEY uses PREV, yes.  We're going from newest to oldest.
-    return defined $last_node->[PREV] ? $last_node->[PREV][KEY]
-                              : undef;
-}
-
-
-sub DESTROY  {
-    my($self) = shift;
-
-    # The chain must be broken.
-    $self->_init;
-    
-    return SUCCESS;
-}
-
-=pod
 
 =back
 
@@ -246,28 +117,6 @@ is immediately truncated.
 
 The size must be an integer greater than or equal to 0.
 
-=cut
-
-sub max_size {
-    my($self) = shift;
-
-    if(@_) {
-        my ($new_max_size) = shift;
-        assert(defined $new_max_size && $new_max_size !~ /\D/);
-        $self->{max_size} = $new_max_size;
-
-        # Immediately purge the cache if necessary.
-        $self->_cull if $self->{size} > $new_max_size;
-
-        return SUCCESS;
-    }
-    else {
-        return $self->{max_size};
-    }
-}
-
-
-=pod
 
 =item B<curr_size>
 
@@ -275,125 +124,20 @@ sub max_size {
 
 Returns the current number of items in the cache.
 
-=cut
 
-sub curr_size {
-    my($self) = shift;
-
-    # We brook no arguments.
-    assert(!@_);
-
-    return $self->{size};
-}
+=back
 
 
-sub _init {
-    my($self) = shift;
+=head1 NOTES
 
-    # The cache is a chain.  We must break up its structure so Perl
-    # can GC it.
-    while( my($key, $node) = each %{$self->{index}} ) {
-        $node->[NEXT] = undef;
-        $node->[PREV] = undef;
-    }
-
-    $self->{freshest}  = undef;
-    $self->{stinkiest} = undef;
-    $self->{index} = {};
-    $self->{size} = 0;
-    
-    return SUCCESS;
-}
+This is just a thin subclass of Tie::Cache::LRU::Array.
 
 
-sub _yank {
-    my($self, $node) = @_;
-    
-    my $prev_node = $node->[PREV];
-    my $next_node = $node->[NEXT];
-    $prev_node->[NEXT] = $next_node if defined $prev_node;
-    $next_node->[PREV] = $prev_node if defined $next_node;
-
-    $node->[NEXT] = undef;
-    $node->[PREV] = undef;
-
-    return SUCCESS;
-}
-
-
-sub _promote {
-    my($self, $node) = @_;
-    
-    # _promote can take a node or a key.  Get the node from the key.
-    $node = $self->{index}{$node} unless ref $node;
-    return unless defined $node;
-    
-    # Don't bother if there's only one node, or if this node is
-    # already the freshest.
-    return if $self->{size} == 1 or $self->{freshest} == $node;
-    
-    # On the off chance that we're about to promote the stinkiest node,
-    # make sure the stinkiest pointer is updated.
-    if( $self->{stinkiest} == $node ) {
-        assert(not defined $node->[PREV]);
-        $self->{stinkiest} = $node->[NEXT];
-    }
-
-    # Pull the $node out of its position.
-    $self->_yank($node);
-    
-    # Place the $node at the head.
-    my $old_head  = $self->{freshest};
-    $old_head->[NEXT]  = $node;
-    $node->[PREV]      = $old_head;
-    $node->[NEXT]      = undef;
-
-    $self->{freshest} = $node;
-    
-    
-    return SUCCESS;
-}
-
-
-sub _cull {
-    my($self) = @_;
-    
-    # Could do this in one step, but it makes sizing the cache by
-    # memory use (not # of items) more difficult.
-    my $max_size = $self->{max_size};
-    for( ;$self->{size} > $max_size; $self->{size}-- ) {
-        my $rotten = $self->{stinkiest};
-        assert(!defined $rotten->[PREV]);
-        my $new_stink = $rotten->[NEXT];
-        
-        $rotten->[NEXT]    = undef;
-        
-        # Gotta watch out for autoviv.
-        $new_stink->[PREV] = undef if defined $new_stink;
-        
-        $self->{stinkiest} = $new_stink;
-        if( $self->{freshest} eq $rotten ) {
-            assert( $self->{size} == 1 ) if DEBUG;
-            $self->{freshest}  = $new_stink;
-        }
-
-        delete $self->{index}{$rotten->[KEY]};
-    }
-    
-    return SUCCESS;
-}
-
-=pod
-
-
-=head1 FUTURE
+=head1 TODO
 
 Should eventually allow the cache to be in shared memory.
 
 Max size by memory use unimplemented.
-
-For small cache sizes, it might be more efficient to just use an array
-instead of a linked list.
 
 
 =head1 AUTHOR
@@ -403,7 +147,8 @@ Michael G Schwern <schwern@pobox.com> for Arena Networks
 
 =head1 SEE ALSO
 
-L<perl(1)>
+L<Tie::Cache::LRU::Array>, L<Tie::Cache::LRU::LinkedList>,
+L<Tie::Cache::LRU::Virtual>, L<Tie::Cache>
 
 =cut
 
